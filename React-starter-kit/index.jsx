833e43d3e1d18250e50a7737931b9688403084af
JuliAlchemDev <julialchem@gmail.com>
2025-11-06 12:30:16 +0100

    feat: add base prestyling, semantic html and bem naming

--- React-starter-kit/index.jsx
+++ React-starter-kit/index.jsx
@@ -1,5 +1,27 @@
 export const App = () => {
-  return(
-    <h1>Hello World!</h1>
+  return (
+    <main className="advice-generator-card">
+      <section className="advice-generator-card__text">
+        <h1 className="advice-generator-card__text-title">Advice #117</h1>
+        <q className="advice-generator-card__text-quotation">It is easy...</q>
+      </section>
+
+      <img
+        src="./assets/images/pattern-divider-mobile.svg"
+        alt=""
+        width="295"
+        height="16"
+        className="advice-generator-card__divider"
+      />
+      <button className="advice-generator-card__button">
+        <img
+          src="./assets/images/icon-dice.svg"
+          alt=""
+          width="24"
+          height="24"
+          className="advice-generator-card__button-img"
+        />
+      </button>
+    </main>
   );
 };
