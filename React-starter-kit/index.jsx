1096a88ff9ad73e9d9cac81ab66d1a6f898eed43
JuliAlchemDev <julialchem@gmail.com>
2025-11-06 13:48:23 +0100

    feat: add fetch advice and get new advice logic

--- React-starter-kit/index.jsx
+++ React-starter-kit/index.jsx
@@ -1,9 +1,38 @@
+const { useState, useEffect } = React;
+
 export const App = () => {
+  const [advice, setAdvice] = useState({});
+  const { id, quotation } = advice;
+
+  const fetchAdvice = async () => {
+    try {
+      const res = await fetch("https://api.adviceslip.com/advice");
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await res.json();
+      setAdvice({
+        id: data.slip.id,
+        quotation: data.slip.advice,
+      });
+    } catch (err) {
+      console.error("No data has been fetched...", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const getNewAdvice = () => {
+    fetchAdvice();
+  };
+
   return (
     <main className="advice-generator-card">
       <section className="advice-generator-card__text">
-        <h1 className="advice-generator-card__text-title">Advice #117</h1>
-        <q className="advice-generator-card__text-quotation">It is easy...</q>
+        <h1 className="advice-generator-card__text-title">Advice #{id}</h1>
+        <q className="advice-generator-card__text-quotation">{quotation}</q>
       </section>
 
       <img
@@ -13,7 +42,11 @@ export const App = () => {
         height="16"
         className="advice-generator-card__divider"
       />
-      <button className="advice-generator-card__button">
+      
+      <button
+        className="advice-generator-card__button"
+        onClick={getNewAdvice}
+      >
         <img
           src="./assets/images/icon-dice.svg"
           alt=""
